373620f0c75745e7a0a47574da2a30a4538ce1c6
sort-nlogn <74429165+sort-nlogn@users.noreply.github.com>
2020-11-22 15:05:39 +0300

    Add files via upload

--- splay_tree.py
+++ splay_tree.py
@@ -110,24 +110,35 @@ def split_(t: Node, x):
     if x < v.key:
         t1 = v.left
         t2 = v
-        v.left.parent = None
-        v.left = None
+        if t1:
+            v.left.parent = None
+        if t2:
+            v.left = None
         return t1, t2
     t1 = v
     t2 = v.right
-    v.right.parent = None
-    v.right = None
+    if t2:
+        v.right.parent = None
+    if t1:
+        v.right = None
     return t1, t2
 
 
 def add(t: Node, data, key):
     t1, t2 = split_(t, key)
     v = Node(key, data, t1, t2)
-    t1.parent = v
-    t2.parent = v
+    if t1:
+        t1.parent = v
+    if t2:
+        t2.parent = v
     return v
 
 
 def remove_(t: Node, x):
     v = find(t, x)
     return merge(v.left, v.right)
+
+t = Node(10, "10")
+a = [5, 11, 22, 48, 2]
+for el in a:
+    t = add(t, str(el), el)
